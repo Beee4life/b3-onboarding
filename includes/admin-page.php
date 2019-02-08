d514c66e915ec2e870a21642b992a16fb8e9c40a
Beee <info@berryplasman.com>
2019-02-08 01:54:45 +0100

    Show default tab

--- includes/admin-page.php
+++ includes/admin-page.php
@@ -19,7 +19,7 @@
                     if ( isset( $_GET[ 'tab' ] ) ) {
                         $default_tab = $_GET[ 'tab' ];
                     } else {
-                        $default_tab = 'settings';
+                        $default_tab = 'pages';
                     }
 
                     $tabs        = array(
