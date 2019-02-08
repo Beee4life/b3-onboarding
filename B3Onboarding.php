89612f4efb2fafb2d72550ac7d8bdf07817b6e21
Beee <info@berryplasman.com>
2019-02-08 01:54:03 +0100

    Version

--- B3Onboarding.php
+++ B3Onboarding.php
@@ -1,7 +1,7 @@
 <?php
     /*
     Plugin Name: B3 - User onboarding
-    Version: 0.0.1
+    Version: 0.5-beta
     Tags: user, management, registration, login, forgot password, reset password, account
     Plugin URI: http://www.berrplasman.com
     Description: This plugin handles the registration/login process
@@ -46,7 +46,7 @@
             function initialize() {
                 $this->settings = array(
                     'path'    => trailingslashit( dirname( __FILE__ ) ),
-                    'version' => '0.0.1',
+                    'version' => '0.5-beta',
                 );
     
                 // set text domain
