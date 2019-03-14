fbd5071019ef0fe5da8f19f91666145ed5ab4bd6
Beee <info@berryplasman.com>
2019-03-14 23:11:43 +0100

    B3 values in debug

--- includes/debug-info.php
+++ includes/debug-info.php
@@ -76,29 +76,32 @@
             </ul>
         </li>
     <?php } ?>
-    
-    <?php $b3_values = b3_get_all_custom_meta_keys(); ?>
-    <ul>
-        <?php
-            if ( ( $key = array_search( 'b3_email_styling', $b3_values ) ) !== false ) {
-                unset( $b3_values[ $key ] );
-            }
-            if ( ( $key = array_search( 'b3_email_template', $b3_values ) ) !== false ) {
-                unset( $b3_values[ $key ] );
-            }
-            foreach( $b3_values as $meta_key ) {
-                echo '<li>';
-                echo $meta_key . ': ';
-                $value = get_option( $meta_key );
-                if ( is_array( $value ) ) {
-                    echo '<pre>' . var_dump( $value ) . '</pre>';
-                } else {
-                    echo ($value) ? $value : esc_html__( 'empty', 'b3-onboarding');
+
+    <li class="debug-list-part">
+        <?php $b3_values = b3_get_all_custom_meta_keys(); ?>
+        <b>B3 values</b>
+        <ul>
+            <?php
+                if ( ( $key = array_search( 'b3_email_styling', $b3_values ) ) !== false ) {
+                    unset( $b3_values[ $key ] );
                 }
-                echo '</li>';
-            }
-        ?>
-    </ul>
+                if ( ( $key = array_search( 'b3_email_template', $b3_values ) ) !== false ) {
+                    unset( $b3_values[ $key ] );
+                }
+                foreach( $b3_values as $meta_key ) {
+                    echo '<li>';
+                    echo $meta_key . ': ';
+                    $value = get_option( $meta_key );
+                    if ( is_array( $value ) ) {
+                        var_dump( $value );
+                    } else {
+                        echo ($value) ? $value : esc_html__( 'empty', 'b3-onboarding');
+                    }
+                    echo '</li>';
+                }
+            ?>
+        </ul>
+    </li>
 </ul>
 
 
