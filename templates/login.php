d6e760be4456b922c30938e3471d52c7b5e377f8
Berry Plasman <berry@totaldesign.com>
2020-05-31 00:31:18 +0200

    Change desc

--- templates/login.php
+++ templates/login.php
@@ -10,7 +10,7 @@
             <?php _e( 'Log In', 'b3-onboarding' ); ?>
         </h2>
     <?php } ?>
-    
+
     <?php
         $reset_page     = get_option( 'b3_forgotpass_page_id' );
         $request_access = get_option( 'b3_registration_type' );
@@ -25,14 +25,14 @@
                 </p>
             <?php } ?>
         <?php } ?>
-        
+
         <?php /* Show success message if user successfully activated */ ?>
         <?php if ( isset( $attributes[ 'user_activate' ] ) ) { ?>
             <p class="b3_message">
                 <?php esc_html_e( 'You have successfully activated your account. You can now log in.', 'b3-onboarding' ); ?>
             </p>
         <?php } ?>
-        
+
         <?php /* Show success message if user successfully registered */ ?>
         <?php if ( $attributes[ 'registered' ] ) { ?>
             <p class="b3_message">
@@ -44,23 +44,23 @@
                         );
                     } else {
                         if ( 'request_access' == $request_access && ! empty( $_GET[ 'registered' ] ) && 'access_requested' == $_GET[ 'registered' ] ) {
-                            
+
                             echo sprintf(
                                 __( 'You have successfully requested access to <strong>%1$s</strong>. You\'ll be notified by email about the result.', 'b3-onboarding' ),
                                 get_bloginfo( 'name' )
                             );
                             $show_form = false;
-                            
+
                         } elseif ( ! empty( $_GET[ 'registered' ] ) && 'confirm_email' == $_GET[ 'registered' ] ) {
-                            
+
                             echo sprintf(
                                 __( 'You have successfully registered to <strong>%1$s</strong>. Please click the confirmation link in your email.', 'b3-onboarding' ),
                                 get_bloginfo( 'name' )
                             );
                             $show_form = false;
-                            
+
                         } else {
-                            
+
                             if ( false == $reset_page ) {
                                 $password_reset_url = esc_url( wp_lostpassword_url() );
                             } else {
@@ -77,22 +77,22 @@
                 ?>
             </p>
         <?php } ?>
-        
-        <?php /* Show message if user reset password */ ?>
+
+        <?php /* Show message if user resets password */ ?>
         <?php if ( $attributes[ 'lost_password_sent' ] ) { ?>
             <p class="b3_message">
                 <?php esc_html_e( 'Check your email for a link to reset your password.', 'b3-onboarding' ); ?>
             </p>
             <?php $show_form = false; ?>
         <?php } ?>
-        
+
         <?php /* Show logged out message if user just logged out */ ?>
         <?php if ( $attributes[ 'logged_out' ] ) { ?>
             <p class="b3_message">
                 <?php esc_html_e( 'You are logged out.', 'b3-onboarding' ); ?>
             </p>
         <?php } ?>
-        
+
         <?php /* Show message if user just reset password */ ?>
         <?php if ( $attributes[ 'password_updated' ] ) { ?>
             <p class="b3_message">
