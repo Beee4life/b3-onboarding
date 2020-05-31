ecd8ab9d4a2c018e29afdcaa5b2bc279c7bfe025
Berry Plasman <berry@totaldesign.com>
2020-05-31 05:16:16 +0200

    Add IF

--- includes/dashboard-widget.php
+++ includes/dashboard-widget.php
@@ -6,12 +6,12 @@
             'role' => 'b3_approval'
         );
         $approval_users = get_users( $approval_args );
-        
+
         $activation_args = array(
             'role' => 'b3_activation'
         );
         $activation_users = get_users( $activation_args );
-    
+
         $all_args = array(
             'exclude' => [ '1' ],
             'number'  => '5',
@@ -19,13 +19,19 @@
             'order'   => 'DESC',
         );
         $all_users = get_users( $all_args );
-        
+
         ?>
         <div class="b3_widget--dashboard">
             <p>
                 <?php
-                    if ( count( $approval_users ) > 0 && 'request_access' == get_option( 'b3_registration_type' ) ) {
-                        echo sprintf( __( 'There %s %d %s awaiting approval. <a href="%s">Click here</a> to manage %s.', 'b3-onboarding' ), _n( 'is', 'are', count( $approval_users ), 'b3-onboarding' ), count( $approval_users ), _n( 'user', 'users', count( $approval_users ), 'b3-onboarding' ), admin_url( 'admin.php?page=b3-user-approval' ), _n( 'this user', 'these users', count( $approval_users ), 'b3-onboarding' ) );
+                    if ( count( $approval_users ) > 0 ) {
+
+                        if ( 'request_access' == get_option( 'b3_registration_type' ) ) {
+                            echo sprintf( __( 'There %s %d %s awaiting approval. <a href="%s">Click here</a> to manage %s.', 'b3-onboarding' ), _n( 'is', 'are', count( $approval_users ), 'b3-onboarding' ), count( $approval_users ), _n( 'user', 'users', count( $approval_users ), 'b3-onboarding' ), admin_url( 'admin.php?page=b3-user-approval' ), _n( 'this user', 'these users', count( $approval_users ), 'b3-onboarding' ) );
+                        } else {
+                            echo sprintf( __( 'There %s %d %s awaiting approval but you changed the registration type and thus also the user approval page.', 'b3-onboarding' ), _n( 'is', 'are', count( $approval_users ), 'b3-onboarding' ), count( $approval_users ), _n( 'user', 'users', count( $approval_users ), 'b3-onboarding' ) );
+                        }
+
                     } elseif ( count( $activation_users ) > 0 ) {
                         echo sprintf( esc_html__( 'There %s %d %s awaiting activation.', 'b3-onboarding' ), _n( 'is', 'are', count( $activation_users ), 'b3-onboarding' ), count( $activation_users ), _n( 'user', 'users', count( $activation_users ), 'b3-onboarding' ) );
                     } else {
@@ -33,7 +39,7 @@
                     }
                 ?>
             </p>
-            
+
             <?php if ( ! empty( $all_users ) ) { ?>
                 <?php $date_time_format = get_option( 'date_format' ) . ' @ ' . get_option( 'time_format' ); ?>
                 <h3>
