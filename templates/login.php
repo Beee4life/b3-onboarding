aa16f4058a7a6f3dada021d0b75a4309530d6d0c
Berry Plasman <berry@totaldesign.com>
2020-06-01 17:18:33 +0200

    Add class

--- templates/login.php
+++ templates/login.php
@@ -101,7 +101,7 @@
         <?php } ?>
 
         <div class="b3_form-element">
-            <label class="b3_form-label" for="user_login"><?php esc_html_e( 'Login or Email address', 'b3-onboarding' ); ?></label>
+            <label class="b3_form-label b3_form-label--userlogin" for="user_login"><?php esc_html_e( 'Username or Email address', 'b3-onboarding' ); ?></label>
             <input type="text" name="log" id="user_login" class="input" value="" size="20">
         </div>
 
