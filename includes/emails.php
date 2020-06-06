f64f5af7c0f7e843f788023152c6c3ce8650774d
Berry Plasman <berry@totaldesign.com>
2020-06-06 03:44:57 +0200

    DRY

--- includes/emails.php
+++ includes/emails.php
@@ -10,7 +10,6 @@
     function b3_render_email_settings_field( $box = false ) {
 
         if ( false != $box ) {
-
             $basic_output = b3_basic_email_settings_field( $box );
             $basic_output = str_replace( '##FOLDOUTCONTENT##', b3_foldout_content( $box ), $basic_output );
 
@@ -65,82 +64,64 @@
         if ( false != $box ) {
 
             ob_start();
-            $output = '';
             switch( $box[ 'id' ] ) {
                 case 'email_settings':
                     include( 'emails/email-settings.php' );
-                    $output = ob_get_clean();
                     break;
                 case 'welcome_email_user':
                     include( 'emails/welcome-email-user.php' );
-                    $output = ob_get_clean();
                     break;
                 case 'new_user_admin':
                     include( 'emails/new-user-admin.php' );
-                    $output = ob_get_clean();
                     break;
                 case 'request_access_user':
                     include( 'emails/request-access-user.php' );
-                    $output = ob_get_clean();
                     break;
                 case 'request_access_admin':
                     include( 'emails/request-access-admin.php' );
-                    $output = ob_get_clean();
                     break;
                 case 'email_activation':
                     include( 'emails/email-activation.php' );
-                    $output = ob_get_clean();
                     break;
                 case 'account_activated':
                     include( 'emails/account-activated.php' );
-                    $output = ob_get_clean();
                     break;
                 case 'account_approved':
                     include( 'emails/account-approved.php' );
-                    $output = ob_get_clean();
                     break;
                 case 'account_rejected':
                     include( 'emails/account-rejected.php' );
-                    $output = ob_get_clean();
                     break;
                 case 'forgot_password':
                     include( 'emails/forgot-password.php' );
-                    $output = ob_get_clean();
                     break;
                 case 'password_changed':
                     // @TODO: check this
                     // include( 'emails/password-changed.php' );
-                    $output = ob_get_clean();
                     break;
                 case 'visitor_register':
                     include( 'emails/ms-visitor-register.php' );
-                    $output = ob_get_clean();
                     break;
                 // Multisite specific
                 case 'visitor_register_site':
                     include( 'emails/ms-visitor-register-site.php' );
-                    $output = ob_get_clean();
                     break;
                 case 'user_registered_site':
                     include( 'emails/ms-user-new-site.php' );
-                    $output = ob_get_clean();
                     break;
                 case 'user_deleted_site':
                     include( 'emails/ms-user-delete-site.php' );
-                    $output = ob_get_clean();
                     break;
                 // Email styling
                 case 'email_styling':
                     include( 'emails/email-styling.php' );
-                    $output = ob_get_clean();
                     break;
                 case 'email_template':
                     include( 'emails/email-template.php' );
-                    $output = ob_get_clean();
                     break;
                 default:
-                    $output = ob_get_clean();
             }
+            $output = ob_get_clean();
 
             return $output;
 
