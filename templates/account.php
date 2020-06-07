7c67c6934e076b8bc632a7d44e72b995582dc1cc
Berry Plasman <berry@totaldesign.com>
2020-06-07 19:09:14 +0200

    Change todo

--- templates/account.php
+++ templates/account.php
@@ -81,7 +81,7 @@
         </table>
 
         <?php
-            // @TODO: check why this IF
+            // @TODO: check why I need this IF
             $show_password_fields = apply_filters( 'show_password_fields', true, $current_user );
             if ( $show_password_fields ) :
         ?>
