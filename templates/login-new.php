4d8e97e82a81023a01e87cf9e28125e499627e84
Berry Plasman <berry@totaldesign.com>
2020-06-10 00:57:50 +0200

    Remove new unused file

--- templates/login-new.php
+++
@@ -1,9 +0,0 @@
-<div id="b3-login" class="b3_page b3_page--login">
-    <?php if ( $attributes[ 'title' ] ) { ?>
-        <h2>
-            <?php _e( 'Log In', 'b3-onboarding' ); ?>
-        </h2>
-    <?php } ?>
-
-    <?php wp_login_form() ; ?>
-</div>
