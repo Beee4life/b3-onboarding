56eb8db2caacbb31f73b05c7877a14c9ad9a3fd5
Berry Plasman <berry@totaldesign.com>
2020-06-18 00:45:47 +0200

    Add docblock

--- includes/download.php
+++ includes/download.php
@@ -1,4 +1,9 @@
 <?php
+    /**
+     * Function to save a file
+     *
+     * @url: https://stackoverflow.com/questions/11664402/save-file-onclick
+     */
     if ( isset( $_GET[ 'file' ] ) ) {
         header("Content-Type: application/octet-stream");
         header("Content-Disposition: attachment; filename={$_GET[ 'file' ]}");
