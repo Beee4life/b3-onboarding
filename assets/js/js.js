2472a6290181cb83188ba10815e1e5979413d229
Beee <info@berryplasman.com>
2020-11-30 05:52:40 +0100

    Show/hide site fields

--- assets/js/js.js
+++ assets/js/js.js
@@ -8,4 +8,13 @@ jQuery(function($){
         $(".b3_message").fadeOut(750);
     });
 
+    var $radio_button = $('.b3_form-element--signup-for input[type="radio"]');
+    $radio_button.change( function() {
+        $subdomain_field = '.b3_form-element--site-fields';
+        if ( 'user' === $(this).val() ) {
+            $($subdomain_field).addClass('hidden');
+        } else {
+            $($subdomain_field).removeClass('hidden');
+        }
+    });
 });
