0ae591ea935942902b0aa89727ac437eabc6e235
Beee <info@berryplasman.com>
2020-11-30 05:54:07 +0100

    Add filter to prevent registation type update

--- includes/filters-b3.php
+++ includes/filters-b3.php
@@ -36,3 +36,41 @@
 
     }
     add_filter( 'b3_extra_fields_validation', 'b3_extra_fields_validation' );
+    
+    
+    /**
+     * Prevent update registration option
+     *
+     * @param $new_value
+     * @param $old_value
+     *
+     * @return false|mixed|string|void
+     */
+    function b3_prevent_update_registration_option( $new_value, $old_value ) {
+        $b3_setting = get_option( 'b3_registration_type' );
+        if ( is_multisite() && is_main_site() ) {
+            if ( 'closed' == $b3_setting ) {
+                $b3_setting = 'none';
+            } elseif ( 'ms_register_user' == $b3_setting ) {
+                $b3_setting = 'user';
+            } elseif ( 'ms_loggedin_register' == $b3_setting ) {
+                $b3_setting = 'blog';
+            } elseif ( 'ms_register_site_user' == $b3_setting ) {
+                $b3_setting = 'all';
+            }
+        } elseif ( ! is_multisite() ) {
+            // @TODO: test this
+            if ( 'closed' == $b3_setting ) {
+                $b3_setting = '0';
+            } elseif ( in_array( $b3_setting, [ 'request_access', 'email_activation', 'open' ] ) ) {
+                $b3_setting = '1';
+            }
+        }
+
+        $new_value = $b3_setting;
+
+        return $new_value;
+        
+    }
+    add_filter( 'pre_update_option_users_can_register', 'b3_prevent_update_registration_option', 10, 2 ); // non-multissite
+    add_filter( 'pre_update_site_option_registration', 'b3_prevent_update_registration_option', 10, 2 ); // multisite
