1fca244179fe513c7c6e46ae295a0f90ebeae501
Beee <info@berryplasman.com>
2020-11-30 06:15:35 +0100

    Add todo

--- includes/emails/ms-user-delete-site.php
+++ includes/emails/ms-user-delete-site.php
@@ -9,21 +9,19 @@
     <tbody>
     <tr>
         <th>
-            <?php // TODO: check name ?>
             <label for="b3__input--password-changed-subject" class=""><?php esc_html_e( 'Email subject', 'b3-onboarding' ); ?></label>
         </th>
         <td>
-            <?php // TODO: check name ?>
+            <?php // TODO: get default value ?>
             <input class="" id="b3__input--password-changed-subject" name="b3_input_password_change_subject" type="text" value="" />
         </td>
     </tr>
     <tr>
         <th class="align-top">
-            <?php // TODO: check name ?>
             <label for="b3__input--password-changed-content" class=""><?php esc_html_e( 'Email content', 'b3-onboarding' ); ?></label>
         </th>
         <td>
-            <?php // TODO: check name ?>
+            <?php // TODO: get default value ?>
             <textarea id="b3__input--password-changed-content" name="b3_input_password_change_content" rows="6"></textarea>
         </td>
     </tr>
