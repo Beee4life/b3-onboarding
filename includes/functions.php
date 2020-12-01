260007353cf338194111631c2b05f8d06babca11
Beee <info@berryplasman.com>
2020-12-01 03:44:39 +0100

    Add docblocks

--- includes/functions.php
+++ includes/functions.php
@@ -1214,6 +1214,13 @@
     }
 
 
+    /**
+     * Get email subject for activate wpmu user (only, no site)
+     *
+     * @since 2.6.0
+     *
+     * @return string|void
+     */
     function b3_get_wpmu_activate_user_subject() {
         $subject = b3_default_wpmu_activate_user_subject();
 
@@ -1221,23 +1228,59 @@
     }
 
 
-    function b3_get_wpmu_activate_user_email() {
-        $message = b3_default_wpmu_activate_user_email();
+    /**
+     * Get email message for activate wpmu user (only, no site)
+     *
+     * @since 2.6.0
+     *
+     * @return string|void
+     */
+    function b3_get_wpmu_activate_user_message() {
+        $message = b3_default_wpmu_activate_user_message();
 
         return $message;
     }
 
 
+    /**
+     * Get email subject for activated wpmu user (only, no site)
+     *
+     * @since 2.6.0
+     *
+     * @return string|void
+     */
     function b3_get_wpmu_user_activated_subject() {
-        $subject = b3_default_wpmu_activate_user_subject();
+        $subject = b3_default_wpmu_user_activated_subject();
 
         return $subject;
     }
 
 
-    // @TODO: not used yet
-    function b3_get_wpmu_user_activated_email() {
-        $message = b3_default_wpmu_user_activated_email();
+    /**
+     * Get email message for activated wpmu user (only, no site)
+     *
+     * @since 2.6.0
+     *
+     * @return string|void
+     */
+    function b3_get_wpmu_user_activated_message() {
+        $message = b3_default_wpmu_user_activated_message();
+
+        return $message;
+    }
+
+
+    /**
+     * Get admin email message for new user
+     *
+     * @since 2.6.0
+     *
+     * @param false $user
+     *
+     * @return string
+     */
+    function b3_get_new_wpmu_user_message_admin( $user = false ) {
+        $message = b3_default_message_new_wpmu_user_admin( $user );
 
         return $message;
     }
