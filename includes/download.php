e98d5fa6cd046f0cf1fffbc626459fbd251aba5e
Beee <info@berryplasman.com>
2021-05-30 14:51:13 +0200

    Refactor

--- includes/download.php
+++ includes/download.php
@@ -6,8 +6,8 @@
      */
     if ( isset( $_GET[ 'sentby' ] ) && isset( $_GET[ 'file' ] ) ) {
         if ( 'b3' == $_GET[ 'sentby' ] ) {
-            header("Content-Type: application/octet-stream");
-            header("Content-Disposition: attachment; filename={$_GET[ 'file' ]}");
-            readfile($_GET[ 'file' ]);
+            header( "Content-Type: application/octet-stream" );
+            header( "Content-Disposition: attachment; filename={$_GET[ 'file' ]}" );
+            readfile( $_GET[ 'file' ] );
         }
     }
