6721037d06ec3c5dca22370e62b76dbf051adbd5
Beee <info@berryplasman.com>
2022-02-28 14:54:13 +0100

    Update filters

--- includes/verify-filters.php
+++ includes/verify-filters.php
@@ -15,6 +15,7 @@
             'b3_account_rejected_message'              => array( 'string' ),
             'b3_account_rejected_subject'              => array( 'string' ),
             'b3_custom_register_inform'                => array( 'string' ),
+            'b3_dashboard_url'                         => array( 'string' ), // @TODO: check
             'b3_disable_action_links'                  => array( 'bool', 'string' ),
             'b3_disallowed_usernames'                  => array( 'array' ),
             'b3_email_activation_message_user'         => array( 'string' ),
@@ -23,14 +24,15 @@
             'b3_email_styling'                         => array( 'string' ),
             'b3_email_template'                        => array( 'string' ),
             'b3_extra_fields'                          => array( 'array', 'extra' ),
+            'b3_extra_fields_validation'               => array( 'array' ), // @TODO: check
             'b3_hidden_fields'                         => array( 'array', 'hidden' ),
+            'b3_link_color'                            => array( 'hex_color' ),
             'b3_localhost'                             => array( 'bool' ),
             'b3_localhost_email'                       => array( 'email' ),
             'b3_localhost_username'                    => array( 'string' ),
             'b3_logged_in_registration_only_message'   => array( 'string' ),
             'b3_lost_password_message'                 => array( 'string' ),
             'b3_lost_password_subject'                 => array( 'string' ),
-            'b3_link_color'                            => array( 'hex_color' ),
             'b3_main_logo'                             => array( 'url', 'file' ),
             'b3_message_above_login'                   => array( 'string' ),
             'b3_message_above_lost_password'           => array( 'string' ),
@@ -41,6 +43,7 @@
             'b3_new_user_notification_addresses'       => array( 'email' ),
             'b3_new_user_subject'                      => array( 'string' ),
             'b3_notification_sender_email'             => array( 'email' ),
+            'b3_notification_sender_name'              => array( 'string' ), // @TODO: check
             'b3_privacy_text'                          => array( 'string' ),
             'b3_redirect_after_register'               => array( 'url' ),
             'b3_register_for'                          => array( 'string' ),
@@ -48,19 +51,20 @@
             'b3_registration_closed_message'           => array( 'string' ),
             'b3_registration_confirm_email_message'    => array( 'string' ),
             'b3_request_access_message_admin'          => array( 'string' ),
-            'b3_request_access_subject_admin'          => array( 'string' ),
             'b3_request_access_message_user'           => array( 'string' ),
+            'b3_request_access_subject_admin'          => array( 'string' ),
             'b3_request_access_subject_user'           => array( 'string' ),
+            'b3_reserved_usernames'                    => array( 'array' ), // @TODO: check
             'b3_signup_for_site'                       => array( 'string' ),
             'b3_signup_for_user'                       => array( 'string' ),
             'b3_welcome_user_message'                  => array( 'string' ),
             'b3_welcome_user_message_manual'           => array( 'string' ),
             'b3_welcome_user_subject'                  => array( 'string' ),
             'b3_widget_links'                          => array( 'array' ),
-            'b3_wpmu_activate_user_subject'            => array( 'string' ),
             'b3_wpmu_activate_user_message'            => array( 'string' ),
-            'b3_wpmu_user_activated_subject'           => array( 'string' ),
+            'b3_wpmu_activate_user_subject'            => array( 'string' ),
             'b3_wpmu_user_activated_message'           => array( 'string' ),
+            'b3_wpmu_user_activated_subject'           => array( 'string' ),
         );
 
         foreach( $custom_filters as $filter => $validation ) {
@@ -81,6 +85,7 @@
                     } else {
                         $error_messages[] = sprintf( __( 'The value, which you set in the filter "%s", is not a string or an array.', 'b3-onboarding' ), $filter );
                     }
+
                 } elseif ( in_array( 'url', $validation ) ) {
                     if ( filter_var( $filter_output, FILTER_VALIDATE_URL ) === false ) {
                         $error_messages[] = sprintf( __( 'The url, which you set in the filter "%s", is not a valid url.', 'b3-onboarding' ), $filter );
@@ -91,6 +96,7 @@
                             }
                         }
                     }
+
                 } elseif ( in_array( 'hex_color', $validation ) ) {
                     if ( false == $filter_output ) {
                         $error_messages[] = sprintf( __( 'The color, which you set in the filter "%s", is not invalid.', 'b3-onboarding' ), $filter );
@@ -99,6 +105,7 @@
                     } elseif ( false == sanitize_hex_color( $filter_output ) ) {
                         $error_messages[] = sprintf( __( 'The color (%s), which you set in the filter "%s", is invalid.', 'b3-onboarding' ), $filter_output, $filter );
                     }
+
                 } elseif ( in_array( 'array', $validation ) ) {
                     if ( ! is_array( $filter_output ) ) {
                         $error_messages[] = sprintf( __( 'The value, which you set in the filter "%s", is not an array.', 'b3-onboarding' ), $filter );
