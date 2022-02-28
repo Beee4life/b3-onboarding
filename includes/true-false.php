54ee5927b96eb167ae35bc01ae109362662f000d
Beee <info@berryplasman.com>
2022-02-28 16:04:25 +0100

    Simplify IF

--- includes/true-false.php
+++ includes/true-false.php
@@ -4,23 +4,18 @@
     }
 
     /**
-     * Check if localhost is defined (not in use yet)
+     * Check if localhost is defined
      *
      * @since 3.1.0
      *
      * @return bool
      */
     function is_localhost() {
-        if ( defined( 'LOCALHOST' ) ) {
-            if ( true == LOCALHOST ) {
-                return true;
-            }
+        if ( defined( 'LOCALHOST' ) && true == LOCALHOST ) {
+            return true;
         } elseif ( true == getenv( 'LOCALHOST' ) ) {
             return true;
-        }
-
-        // @TODO: document
-        if ( apply_filters( 'b3_localhost', false ) ) {
+        } elseif ( apply_filters( 'b3_localhost', false ) ) {
             return true;
         }
 
