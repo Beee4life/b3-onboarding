8bc36c7f020ca04de97323b7a76c9444c70db39b
Beee <info@berryplasman.com>
2022-03-01 10:55:55 +0100

    Add log

--- includes/filters-wp.php
+++ includes/filters-wp.php
@@ -334,6 +334,9 @@
                 $errors->add( 'last_name_error', sprintf( '<strong>%s</strong>: %s', __( 'ERROR', 'b3-onboarding' ), __( 'You must include a last name.', 'b3-onboarding' ) ) );
             }
         }
+        error_log('HIT');
+
+        // @TODO: check on MS
         if ( 1 == get_option( 'b3_activate_recaptcha' ) ) {
             $b3ob = new B3Onboarding();
             if ( ! $b3ob->b3_verify_recaptcha() ) {
