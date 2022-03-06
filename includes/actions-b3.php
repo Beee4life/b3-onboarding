8fee6551c8b368b2172e9af21e955cfab12e0777
Beee <info@berryplasman.com>
2022-03-06 03:54:15 +0100

    Remove dummy value

--- includes/actions-b3.php
+++ includes/actions-b3.php
@@ -159,7 +159,7 @@
             </div>
             <div class="b3_form-element b3_form-element--email">
                 <label class="b3_form-label" for="b3_user_email"><?php esc_html_e( 'Email', 'b3-onboarding' ); ?> <strong>*</strong></label>
-                <input type="email" name="user_email" id="b3_user_email" class="b3_form--input" value="<?php echo is_localhost() ? apply_filters( 'b3_localhost_email', 'dummy@email.com' ) : ''; ?>" required>
+                <input type="email" name="user_email" id="b3_user_email" class="b3_form--input" value="<?php echo is_localhost() ? apply_filters( 'b3_localhost_email', false ) : ''; ?>" required>
             </div>
 
             <?php if ( 'all' == $registration_type ) { ?>
