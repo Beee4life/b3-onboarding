e21177df7db5d1139063f7699a9b7702052e5372
Beee <info@berryplasman.com>
2022-03-29 19:53:52 +0200

    Add label class

--- includes/actions-b3.php
+++ includes/actions-b3.php
@@ -401,7 +401,7 @@
             do_action( 'b3_do_before_privacy_checkbox' );
             $input = '<input name="b3_privacy_accept" type="checkbox" id="b3_privacy_accept" value="1"/>';
             $label = htmlspecialchars_decode( apply_filters( 'b3_privacy_text', b3_get_privacy_text() ) );
-            echo sprintf( '<div class="b3_form-element b3_form-element--privacy"><label>%s</label> %s</div>', $label, $input );
+            echo sprintf( '<div class="b3_form-element b3_form-element--privacy"><label class="b3_form-label">%s</label> %s</div>', $label, $input );
             do_action( 'b3_do_after_privacy_checkbox' );
         }
     }
