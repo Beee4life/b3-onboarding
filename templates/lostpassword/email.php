f1125ed15c102eebfb81ee8c422680317b86203e
Beee <info@berryplasman.com>
2022-03-31 19:16:54 +0200

    Apply filter

--- templates/lostpassword/email.php
+++ templates/lostpassword/email.php
@@ -5,5 +5,5 @@
 ?>
 <div class="b3_form-element b3_form-element--email">
     <label class="b3_form-label b3_form-label--email" for="b3_user_email"><?php esc_attr_e( 'Email address', 'b3-onboarding' ); ?></label>
-    <input type="text" name="user_login" id="b3_user_email" value="" required>
+    <input type="text" name="user_login" id="b3_user_email" value="<?php echo apply_filters( 'b3_localhost_email', false ); ?>" required>
 </div>
