0b378155e3d002f30e22206306e762aea70fbda2
Beee <info@berryplasman.com>
2022-04-02 15:29:14 +0200

    Add div

--- includes/actions-b3.php
+++ includes/actions-b3.php
@@ -290,8 +290,10 @@
                         <?php $current_network = get_network(); ?>
                         <?php if ( is_subdomain_install() ) { ?>
                             <label class="b3_form-label" for="blogname"><?php esc_html_e( 'Site (sub) domain', 'b3-onboarding' ); ?></label>
-                            <input name="blogname" id="blogname" value="<?php echo apply_filters( 'b3_localhost_blogname', false ); ?>" type="text" class="b3_form--input" placeholder="<?php esc_html_e( 'customdomain', 'b3-onboarding' ); ?>" />
-                            .<?php echo $_SERVER[ 'HTTP_HOST' ]; ?>
+                            <div>
+                                <input name="blogname" id="blogname" value="<?php echo apply_filters( 'b3_localhost_blogname', false ); ?>" type="text" class="b3_form--input" placeholder="<?php esc_html_e( 'customdomain', 'b3-onboarding' ); ?>" />
+                                <div class="b3_site-domain">.<?php echo $_SERVER[ 'HTTP_HOST' ]; ?></div>
+                            </div>
                         <?php } else { ?>
                             <label class="b3_form-label" for="blogname"><?php esc_html_e( 'Site address', 'b3-onboarding' ); ?></label>
                             <?php echo $current_network->domain . $current_network->path; ?><input name="blogname" id="blogname" value="" type="text" class="b3_form--input" placeholder="<?php esc_html_e( 'address', 'b3-onboarding' ); ?>" />
