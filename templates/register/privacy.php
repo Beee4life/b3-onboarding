e9df59d5381ccac440c1b81628ba2610e9970b1d
Beee <info@berryplasman.com>
2022-04-03 00:09:57 +0200

    Rename file

--- templates/register/privacy.php
+++ templates/register/privacy.php
@@ -3,8 +3,6 @@
         exit;
     }
 
-    do_action( 'b3_do_before_privacy_checkbox' );
     $input = '<input name="b3_privacy_accept" type="checkbox" id="b3_privacy_accept" value="1"/>';
     $label = htmlspecialchars_decode( apply_filters( 'b3_privacy_text', b3_get_privacy_text() ) );
     echo sprintf( '<div class="b3_form-element b3_form-element--privacy"><label class="b3_form-label">%s</label> %s</div>', $label, $input );
-    do_action( 'b3_do_after_privacy_checkbox' );
