f2d0d578fccef30c8c5049cce5650a116d6af3ae
Beee <info@berryplasman.com>
2022-04-03 01:11:52 +0200

    Change string

--- includes/emails/request-access-admin.php
+++ includes/emails/request-access-admin.php
@@ -23,7 +23,7 @@
     </tr>
     <tr>
         <td colspan="2">
-            <?php esc_html_e( 'If a field is left empty the placeholder will be used.', 'b3-onboarding' ); ?>
+            <?php esc_html_e( 'If a field is left empty the default value will be used.', 'b3-onboarding' ); ?>
         </td>
     </tr>
     <tr>
