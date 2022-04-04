2c963bae739b2de4fe4927933be3705fbf7b7cab
Beee <info@berryplasman.com>
2022-04-04 18:18:11 +0200

    Change field name

--- templates/register/user-login.php
+++ templates/register/user-login.php
@@ -5,5 +5,5 @@
 ?>
 <div class="b3_form-element b3_form-element--login">
     <label class="b3_form-label" for="b3_user_login"><?php esc_html_e( 'User name', 'b3-onboarding' ); ?> <strong>*</strong></label>
-    <input type="text" name="user_name" id="b3_user_login" class="b3_form--input" autocapitalize="none" autocomplete="off" spellcheck="false" maxlength="60" value="<?php echo apply_filters( 'b3_localhost_username', false ); ?>" required>
+    <input type="text" name="user_login" id="b3_user_login" class="b3_form--input" autocapitalize="none" autocomplete="off" spellcheck="false" maxlength="60" value="<?php echo apply_filters( 'b3_localhost_username', false ); ?>" required>
 </div>
