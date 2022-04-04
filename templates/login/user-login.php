f9887008565fa9ae215d03e10327fad831ac73dc
Beee <info@berryplasman.com>
2022-04-04 23:24:08 +0200

    Dynamic field name

--- templates/login/user-login.php
+++ templates/login/user-login.php
@@ -2,14 +2,14 @@
     if ( ! defined( 'ABSPATH' ) ) {
         exit;
     }
+    $field_name = is_multisite() ? 'user_name' : 'user_login';
 
     $label = esc_html__( 'Username or Email address', 'b3-onboarding' );
     if ( 1 == get_option( 'b3_register_email_only' ) ) {
         $label = esc_html__( 'Email address', 'b3-onboarding' );
     }
-
 ?>
 <div class="b3_form-element">
     <label class="b3_form-label b3_form-label--userlogin" for="user_login"><?php echo $label; ?></label>
-    <input type="text" name="log" id="user_login" class="input" value="" size="20" autocomplete="username">
+    <input type="text" name="<?php echo $field_name; ?>" id="user_login" class="input" value="" size="20" autocomplete="username">
 </div>
