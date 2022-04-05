6469cf6a8d759300e05874d4da4f9fb13b3e9e74
Beee <info@berryplasman.com>
2022-04-05 20:24:14 +0200

    Back to log

--- templates/login/user-login.php
+++ templates/login/user-login.php
@@ -2,8 +2,6 @@
     if ( ! defined( 'ABSPATH' ) ) {
         exit;
     }
-    $field_name = is_multisite() ? 'log' : 'user_login';
-
     $label = esc_html__( 'Username or Email address', 'b3-onboarding' );
     if ( 1 == get_option( 'b3_register_email_only' ) ) {
         $label = esc_html__( 'Email address', 'b3-onboarding' );
@@ -11,5 +9,5 @@
 ?>
 <div class="b3_form-element">
     <label class="b3_form-label b3_form-label--userlogin" for="user_login"><?php echo $label; ?></label>
-    <input type="text" name="<?php echo $field_name; ?>" id="user_login" class="input" value="" size="20" autocomplete="username">
+    <input type="text" name="log" id="user_login" class="input" value="" size="20" autocomplete="username">
 </div>
