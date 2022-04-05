ebe0e88728ebc3fb56eb36604d09c279d91abefd
Beee <info@berryplasman.com>
2022-04-05 22:28:58 +0200

    Change IF for filter

--- B3Onboarding.php
+++ B3Onboarding.php
@@ -1210,8 +1210,10 @@
                     }
                     if ( isset( $show_warning ) && $show_warning ) {
                         $warning_message = sprintf( esc_html__( "You're using a development version of %s, which has not been released yet and can give some unexpected results.", 'b3-onboarding' ), 'B3 OnBoarding' );
-                        $notice = sprintf( '<div class="notice notice-warning"><p>%s</p></div>', $warning_message );
-                        echo apply_filters( 'b3_hide_development_notice', $notice );
+                        $notice          = sprintf( '<div class="notice notice-warning"><p>%s</p></div>', $warning_message );
+                        if ( false == apply_filters( 'b3_hide_development_notice', false ) ) {
+                            echo $notice;
+                        }
                     }
                 }
 
