b258f8955bb64719d4bfbec009617d706d382cf8
Beee <info@berryplasman.com>
2022-04-06 22:18:09 +0200

    Change button text + remove commented lines

--- includes/tabs/tab-template.php
+++ includes/tabs/tab-template.php
@@ -41,8 +41,6 @@
             <?php foreach( $email_boxes as $box ) { ?>
                 <div class="template_box">
                     <label for="b3__input--<?php echo $box['id']; ?>"><?php echo $box['title']; ?></label>
-<!--                    <br>-->
-                        <?php //echo sprintf(  '<a href="%s">%s</a>', esc_url( B3OB_PLUGIN_URL . 'includes/download.php?file=default-email-template.html&sentby=b3' ), esc_html__( 'Download template', 'b3-onboarding' ) ); ?>
                     <textarea id="b3__input--<?php echo $box['id']; ?>" name="b3_<?php echo $box['id']; ?>" placeholder="<?php echo esc_attr( b3_default_email_template() ); ?>" rows="6"><?php echo $box[ 'value' ]; ?></textarea>
                     <p>
                         <?php echo b3_get_preview_link( $box['preview'] ); ?> <small>(<?php esc_html_e( 'opens in new window', 'b3-onboarding' ); ?></small>
@@ -52,7 +50,7 @@
                 </div>
             <?php } ?>
         
-            <?php b3_get_submit_button(); ?>
+            <?php b3_get_submit_button( esc_attr__( 'Save templates', 'b3-onboarding' ) ); ?>
         </form>
 
         <?php
