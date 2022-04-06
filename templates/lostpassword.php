0f8a7d625fb7dc705eb5e1ab01e344ca857072db
Beee <info@berryplasman.com>
2022-04-06 02:01:47 +0200

    Rename template

--- templates/lostpassword.php
+++ templates/lostpassword.php
@@ -16,7 +16,7 @@
 
     <form name="lostpasswordform" id="lostpasswordform" class="b3_form b3_form--lostpass" action="<?php echo b3_get_current_url(); ?>" method="post">
         <?php do_action( 'b3_render_form_element', 'lostpassword/hidden-fields', $attributes ); ?>
-        <?php do_action( 'b3_render_form_element', 'lostpassword/email', $attributes ); ?>
+        <?php do_action( 'b3_render_form_element', 'lostpassword/user-login', $attributes ); ?>
         <?php do_action( 'b3_render_form_element', 'general/button', $attributes ); ?>
         <?php do_action( 'b3_add_action_links', $attributes[ 'template' ] ); ?>
     </form>
