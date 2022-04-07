63312ca800b86f1d12ed9056ec3022042b8cd035
Beee <info@berryplasman.com>
2022-04-07 05:10:56 +0200

    Change name

--- includes/form-handling.php
+++ includes/form-handling.php
@@ -62,7 +62,7 @@
                     delete_option( 'b3_activate_first_last' );
                     delete_option( 'b3_first_last_required' );
                 }
-    
+
                 if ( isset( $_POST[ 'b3_register_email_only' ] ) && 1 == $_POST[ 'b3_register_email_only' ] ) {
                     update_option( 'b3_register_email_only', 1, false );
                     delete_option( 'b3_activate_first_last' );
@@ -70,7 +70,7 @@
                 } else {
                     delete_option( 'b3_register_email_only' );
                 }
-    
+
                 if ( isset( $_POST[ 'b3_redirect_set_password' ] ) && 1 == $_POST[ 'b3_redirect_set_password' ] ) {
                     update_option( 'b3_redirect_set_password', 1, false );
                 } else {
@@ -176,7 +176,7 @@
                 if ( isset( $_POST[ 'b3_notification_sender_name' ] ) && ! empty( $_POST[ 'b3_notification_sender_name' ] ) ) {
                     update_option( 'b3_notification_sender_name', $_POST[ 'b3_notification_sender_name' ], false );
                 } else {
-                    delete_option( 'b3_notification_sender_email' );
+                    delete_option( 'b3_notification_sender_name' );
                 }
 
                 if ( isset( $_POST[ 'b3_activate_custom_emails' ] ) && 1 == $_POST[ 'b3_activate_custom_emails' ] ) {
@@ -390,33 +390,33 @@
         }
     }
     add_action( 'init', 'b3_email_form_handling', 1 );
-    
-    
+
+
     function b3_template_form_handling() {
         if ( 'POST' == $_SERVER[ 'REQUEST_METHOD' ] && isset( $_POST[ 'b3_template_nonce' ] ) ) {
             if ( ! wp_verify_nonce( $_POST[ 'b3_template_nonce' ], 'b3-template-nonce' ) ) {
                 B3Onboarding::b3_errors()->add( 'error_no_nonce_match', esc_html__( 'Something went wrong, please try again.', 'b3-onboarding' ) );
-                
+
             } else {
                 if ( isset( $_POST[ 'b3_email_styling' ] ) && ! empty( $_POST[ 'b3_email_styling' ] ) ) {
                     update_option( 'b3_email_styling', stripslashes( $_POST[ 'b3_email_styling' ] ), false );
                 } else {
                     delete_option( 'b3_email_styling' );
                 }
-                
+
                 if ( isset( $_POST[ 'b3_email_template' ] ) && ! empty( $_POST[ 'b3_email_template' ] ) ) {
                     update_option( 'b3_email_template', stripslashes( $_POST[ 'b3_email_template' ] ), false );
                 } else {
                     delete_option( 'b3_email_template' );
                 }
-                
+
                 B3Onboarding::b3_errors()->add( 'success_template_saved', esc_html__( 'Template settings saved', 'b3-onboarding' ) );
             }
         }
     }
     add_action( 'init', 'b3_template_form_handling', 1 );
-    
-    
+
+
     /**
      * Form handling for user settings
      *
@@ -438,7 +438,7 @@
                 if ( ! is_multisite() ) {
                     if ( isset( $_POST[ 'b3_restrict_usernames' ] ) && 1 == $_POST[ 'b3_restrict_usernames' ] ) {
                         update_option( 'b3_restrict_usernames', 1, false );
-                        
+
                         if ( isset( $_POST[ 'b3_disallowed_usernames' ] ) && ! empty( $_POST[ 'b3_disallowed_usernames' ] ) ) {
                             $sanitized_value = sanitize_text_field( $_POST[ 'b3_disallowed_usernames' ] );
                             $new_value       = explode( ' ', $sanitized_value );
@@ -455,7 +455,7 @@
                 // @TODO: check if should be kept out of MS
                 if ( isset( $_POST[ 'b3_domain_restrictions' ] ) && ! empty( $_POST[ 'b3_domain_restrictions' ] ) ) {
                     update_option( 'b3_domain_restrictions', $_POST[ 'b3_domain_restrictions' ], false );
-                    
+
                     if ( isset( $_POST[ 'b3_disallowed_domains' ] ) && ! empty( $_POST[ 'b3_disallowed_domains' ] ) ) {
                         $sanitized_value = sanitize_text_field( $_POST[ 'b3_disallowed_domains' ] );
                         $new_value       = explode( ' ', $sanitized_value );
@@ -546,13 +546,13 @@
                 B3Onboarding::b3_errors()->add( 'error_no_nonce_match', esc_html__( 'Something went wrong, please try again.', 'b3-onboarding' ) );
             } else {
                 $reset = false;
-    
+
                 if ( ! empty( $_POST[ 'b3_link_color' ] ) ) {
                     update_option( 'b3_link_color', $_POST[ 'b3_link_color' ], false );
                 } else {
                     delete_option( 'b3_link_color' );
                 }
-        
+
                 if ( isset( $_POST[ 'b3_disable_action_links' ] ) && 1 == $_POST[ 'b3_disable_action_links' ] ) {
                     update_option( 'b3_disable_action_links', 1, false );
                 } else {
