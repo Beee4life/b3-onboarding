55f72aec436ede6238ca94825159bb372908e0a0
Beee <info@berryplasman.com>
2022-04-18 17:55:34 +0200

    Move field

--- includes/tabs/tab-users.php
+++ includes/tabs/tab-users.php
@@ -85,6 +85,18 @@
                 </div>
             <?php b3_get_close(); ?>
 
+            <?php if ( ! is_multisite() ) { ?>
+				<?php b3_get_settings_field_open(); ?>
+                    <?php b3_get_label_field_open(); ?>
+                        <label for="b3_hide_admin_bar"><?php esc_html_e( 'Hide admin bar', 'b3-onboarding' ); ?></label>
+                    <?php b3_get_close(); ?>
+                    <div class="b3_settings-input b3_settings-input--checkbox">
+                        <input type="checkbox" id="b3_hide_admin_bar" name="b3_hide_admin_bar" value="1" <?php checked($hide_admin_bar); ?>/>
+                        <?php esc_html_e( "Hide the admin bar for user roles which don't have admin access.", 'b3-onboarding' ); ?>
+                    </div>
+				<?php b3_get_close(); ?>
+            <?php } ?>
+
             <?php if ( ! is_multisite() && 'none' != $registration_type ) { ?>
                 <?php
                     $email_only                 = get_option( 'b3_register_email_only' );
@@ -141,16 +153,6 @@
             <?php } ?>
 
             <?php if ( ! is_multisite() ) { ?>
-                <?php b3_get_settings_field_open(); ?>
-                    <?php b3_get_label_field_open(); ?>
-                        <label for="b3_hide_admin_bar"><?php esc_html_e( 'Hide admin bar', 'b3-onboarding' ); ?></label>
-                    <?php b3_get_close(); ?>
-                    <div class="b3_settings-input b3_settings-input--checkbox">
-                        <input type="checkbox" id="b3_hide_admin_bar" name="b3_hide_admin_bar" value="1" <?php checked($hide_admin_bar); ?>/>
-                        <?php esc_html_e( "Hide the admin bar for user roles which don't have admin access.", 'b3-onboarding' ); ?>
-                    </div>
-                <?php b3_get_close(); ?>
-
                 <?php b3_get_settings_field_open(); ?>
                     <?php b3_get_label_field_open(); ?>
                         <label for="b3_user_may_delete"><?php esc_html_e( 'User may delete account', 'b3-onboarding' ); ?></label>
