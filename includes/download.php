0abfee07c701ac984eae12a5989c67039495ce2c
Beee <info@berryplasman.com>
2022-07-27 19:23:19 +0200

    Limit to certain files

--- includes/download.php
+++ includes/download.php
@@ -5,9 +5,17 @@
      * @url: https://stackoverflow.com/questions/11664402/save-file-onclick
      */
     if ( isset( $_GET[ 'sentby' ] ) && isset( $_GET[ 'file' ] ) ) {
-        if ( 'b3' == $_GET[ 'sentby' ] ) {
-            header( "Content-Type: application/octet-stream" );
-            header( "Content-Disposition: attachment; filename={$_GET[ 'file' ]}" );
-            readfile( $_GET[ 'file' ] );
-        }
+		$allowed_files = [
+			'default-email-styling.css',
+			'default-email-template.html',
+		];
+
+		// @TODO: sanitize file name
+		$file_name = $_GET[ 'file' ];
+
+		if ( file_exists( $file_name ) && is_readable( $file_name ) && in_array( $file_name, $allowed_files ) ) {
+			header( "Content-Type: application/octet-stream" );
+			header( "Content-Disposition: attachment; filename={$file_name}" );
+			readfile( $file_name );
+		}
     }
