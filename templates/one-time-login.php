1e4c09be8a964083a80acea575cbda570ae669e0
Beee <info@berryplasman.com>
2024-06-17 21:11:25 +0200

    Enable

--- templates/one-time-login.php
+++ templates/one-time-login.php
@@ -8,7 +8,7 @@
 <form name="loginform" id="loginform" action="<?php echo esc_url( site_url( '?custom_login' ) ); ?>" method="post">
     <?php // Output of fields starts here ?>
     <?php do_action( 'b3_render_form_element', 'login/hidden-fields', $attributes ); ?>
-    <?php //do_action( 'b3_render_form_element', 'login/one-time-login' ); ?>
+    <?php do_action( 'b3_render_form_element', 'login/one-time-login' ); ?>
     <?php do_action( 'b3_render_form_element', 'general/button', $attributes ); ?>
     <?php do_action( 'b3_add_action_links', $attributes[ 'template' ] ); ?>
 </form>
