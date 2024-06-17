3ed111d7b4b2446d84f8da17291d90dff60a9c8f
Beee <info@berryplasman.com>
2024-06-17 20:59:36 +0200

    Change check

--- templates/login.php
+++ templates/login.php
@@ -12,7 +12,7 @@
     do_action( 'b3_add_form_messages', $attributes );
 ?>
 <div id="b3-login" class="b3_page b3_page--login">
-    <?php if ( isset( $attributes[ 'one_time_password' ] ) && true == $attributes[ 'one_time_password' ] ) { ?>
+    <?php if ( isset( $attributes[ 'one_time_password' ] ) && '1' == $attributes[ 'one_time_password' ] ) { ?>
         <?php include 'one-time-login.php'; ?>
     <?php } else { ?>
         <?php include 'wp-login.php'; ?>
