6abc14259b559e7fccd529b4f280921dbdbfdaeb
Beee <info@berryplasman.com>
2024-06-18 02:55:06 +0200

    Change include

--- templates/login.php
+++ templates/login.php
@@ -13,7 +13,7 @@
 ?>
 <div id="b3-login" class="b3_page b3_page--login">
     <?php if ( isset( $attributes[ 'one_time_password' ] ) && '1' == $attributes[ 'one_time_password' ] ) { ?>
-        <?php include 'one-time-login.php'; ?>
+        <?php include 'getpass/one-time-password.php'; ?>
     <?php } else { ?>
         <?php include 'wp-login.php'; ?>
     <?php } ?>
