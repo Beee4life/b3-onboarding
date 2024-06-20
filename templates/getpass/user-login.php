74f3f217fcac20302f038f78a702ad2177591ebf
Beee <info@berryplasman.com>
2024-06-21 00:43:46 +0200

    Add filter

--- templates/getpass/user-login.php
+++ templates/getpass/user-login.php
@@ -6,5 +6,5 @@
 ?>
 <div class="b3_form-element">
     <label class="b3_form-label b3_form-label--useremail" for="user_email"><?php echo $label; ?></label>
-    <input type="email" name="email" id="user_email" class="input" value="" size="20" autocomplete="username">
+    <input type="email" name="email" id="user_email" class="input" value="<?php echo apply_filters( 'b3_localhost_email', false ); ?>   " size="20" autocomplete="username">
 </div>
