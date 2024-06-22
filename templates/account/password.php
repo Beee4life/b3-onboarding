cc47d38501f592e772f27d366f51c3ffaf5d6887
Beee <info@berryplasman.com>
2024-06-22 14:57:12 +0200

    Add div to style better

--- templates/account/password.php
+++ templates/account/password.php
@@ -10,7 +10,9 @@
             <div class="password-input user-pass1-wrap">
                 <!-- Workaround : https://core.trac.wordpress.org/ticket/24364 -->
                 <input class="hidden" value=" " />
-                <button type="button" class="button button-secondary button--password wp-generate-pw hide-if-no-js"><?php esc_attr_e( 'Change password', 'b3-onboarding' ); ?></button>
+                <div class="change-password-button">
+                    <button type="button" class="button button-secondary button--password wp-generate-pw hide-if-no-js"><?php esc_attr_e( 'Change password', 'b3-onboarding' ); ?></button>
+                </div>
                 <div class="wp-pwd hide-if-js">
                     <div class="password-input">
                         <label class="b3_form-label" for="pass1">
