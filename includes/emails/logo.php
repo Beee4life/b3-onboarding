b6cd2830476d09c243b5989f816628feb3b80673
Beee <info@berryplasman.com>
2024-06-22 22:17:45 +0200

    Output logo

--- includes/emails/logo.php
+++ includes/emails/logo.php
@@ -10,27 +10,36 @@
     <tbody>
     <tr>
         <td colspan="2" class="b3__intro">
-            <?php esc_html_e( 'This is the logo used in email headers.', 'b3-onboarding' ); ?>
+            <?php esc_html_e( 'This is the logo used in email headers using the default template.', 'b3-onboarding' ); ?>
         </td>
     </tr>
     <tr>
-        <th>
+        <th class="align-top">
             <label for="b3_main_logo"><?php esc_html_e( 'Logo', 'b3-onboarding' ); ?></label>
         </th>
         <td>
             <?php if ( false == apply_filters( 'b3_main_logo', false ) ) { ?>
-                <label>
-                    <input type="url" name="b3_main_logo" id="b3_main_logo" value="<?php echo esc_url( $main_logo ); ?>" />
-                </label>
-                <a href="#" id="main-logo" class="b3-open-media button button-primary" title="<?php esc_attr_e( 'Choose a logo', 'b3-onboarding' ); ?>">
-                    <?php esc_html_e( 'Choose a logo', 'b3-onboarding' ); ?>
-                </a>
+                <div class="logo-fields">
+                    <div>
+                        <label>
+                            <input type="url" name="b3_main_logo" id="b3_main_logo" value="<?php echo esc_url( $main_logo ); ?>" />
+                        </label>
+                    </div>
+                    <div>
+                        <a href="#" id="main-logo" class="b3-open-media button button-primary" title="<?php esc_attr_e( 'Choose a logo', 'b3-onboarding' ); ?>">
+                            <?php esc_html_e( 'Choose a logo', 'b3-onboarding' ); ?>
+                        </a>
+                    </div>
+                </div>
+                <?php if ( $main_logo ) { ?>
+                    <div>
+                        <img src="<?php echo $main_logo; ?>" alt="Your logo" class="preview-logo" style="max-width: 150px;" />
+                    </div>
+                <?php } ?>
             <?php } else { ?>
                 <?php esc_html_e( "You've set this logo with a filter.", 'b3-onboarding' ); ?>
                 <br>
-                <a href="<?php echo apply_filters( 'b3_main_logo', false ); ?>">
-                    <img src="<?php echo apply_filters( 'b3_main_logo', false ); ?>" alt="" style="max-width: 300px;" />
-                </a>
+                <img src="<?php echo apply_filters( 'b3_main_logo', false ); ?>" alt="" style="max-width: 300px;" />
             <?php } ?>
         </td>
     </tr>
