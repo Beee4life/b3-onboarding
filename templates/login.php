be8aa7e0e99492f11ce72e6fed1482261374b096
Beee <info@berryplasman.com>
2024-06-23 19:56:23 +0200

    Remove IF

--- templates/login.php
+++ templates/login.php
@@ -12,9 +12,5 @@
     do_action( 'b3_add_form_messages', $attributes );
 ?>
 <div id="b3-login" class="b3_page b3_page--login">
-    <?php if ( isset( $attributes[ 'one_time_password' ] ) && '1' == $attributes[ 'one_time_password' ] ) { ?>
-        <?php include 'getpass/one-time-password.php'; ?>
-    <?php } else { ?>
-        <?php include 'wp-login.php'; ?>
-    <?php } ?>
+    <?php include 'wp-login.php'; ?>
 </div>
