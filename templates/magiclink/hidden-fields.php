227f7a907bd742ca6525ba09895285b673fd82cd
Beee <info@berryplasman.com>
2024-06-23 19:56:18 +0200

    Remove hidden email

--- templates/magiclink/hidden-fields.php
+++ templates/magiclink/hidden-fields.php
@@ -3,9 +3,5 @@
         exit;
     }
 ?>
-<input name="b3_form" value="getpass" type="hidden" />
+<input name="b3_form" value="magiclink" type="hidden" />
 <input name="<?php echo $attributes[ 'nonce_id' ]; ?>" value="<?php echo $attributes[ 'nonce' ]; ?>" type="hidden" />
-
-<?php if ( isset( $attributes[ 'email' ] ) ) { ?>
-    <input name="email" type="hidden" value="<?php esc_attr_e( $attributes[ 'email' ] ); ?>" />
-<?php } ?>
