d3f537e460c032f3e3804fc8b3645a27ce6eab44
Beee <info@berryplasman.com>
2024-06-23 19:56:35 +0200

    Change form name + id

--- templates/magiclink.php
+++ templates/magiclink.php
@@ -10,12 +10,12 @@
     }
     do_action( 'b3_add_form_messages', $attributes );
 ?>
-<div id="b3-resetpass" class="b3 b3_page b3_page--getpass">
+<div id="b3-resetpass" class="b3 b3_page b3_page--magiclink">
     <?php echo ( isset( $attributes[ 'title' ] ) ) ? sprintf( '<h3>%s</h3>', $attributes[ 'title' ] ) : false; ?>
 
-    <form name="getpassform" id="getpassform" action="<?php echo $attributes['form_action']; ?>" method="post" autocomplete="off">
-        <?php do_action( 'b3_render_form_element', 'getpass/hidden-fields', $attributes ); ?>
-        <?php do_action( 'b3_render_form_element', 'getpass/user-login', $attributes ); ?>
+    <form name="magiclinkform" id="magiclinkform" action="<?php echo $attributes['form_action']; ?>" method="post" autocomplete="off">
+        <?php do_action( 'b3_render_form_element', 'magiclink/hidden-fields', $attributes ); ?>
+        <?php do_action( 'b3_render_form_element', 'magiclink/user-email', $attributes ); ?>
         <?php do_action( 'b3_render_form_element', 'general/button', $attributes ); ?>
     </form>
 </div>
