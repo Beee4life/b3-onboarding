f8cd436f56ded1579c133c3118a275639cf6adcc
Beee <info@berryplasman.com>
2024-07-06 14:26:03 +0200

    Change todo

--- includes/functions.php
+++ includes/functions.php
@@ -69,7 +69,7 @@
         if ( is_main_site() ) {
             if ( is_multisite() ) {
                 if ( in_array( $registration_type, [ 'user' ] ) ) {
-                    // @TODO: test this
+                    // @TODO (!): test this
                     // $email_boxes[] = array(
                     //     'id'    => 'email_activation',
                     //     'title' => esc_html__( 'Email activation (user)', 'b3-onboarding' ),
