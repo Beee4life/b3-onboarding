3489b8fe7eefd3f4716c70ce77c13f1f8e0df934
Beee <info@berryplasman.com>
2024-10-22 00:12:35 +0200

    Prevent empty titles

--- templates/lostpassword.php
+++ templates/lostpassword.php
@@ -12,7 +12,9 @@
     do_action( 'b3_add_form_messages', $attributes );
 ?>
 <div class="b3_page b3_page--lostpass">
-    <?php echo ( isset( $attributes[ 'title' ] ) ) ? sprintf( '<h3>%s</h3>', $attributes[ 'title' ] ) : false; ?>
+    <?php if ( ! empty( $attributes[ 'title' ] ) ) { ?>
+        <?php echo sprintf( '<h3>%s</h3>', $attributes[ 'title' ] ); ?>
+    <?php } ?>
 
     <form name="lostpasswordform" id="lostpasswordform" class="b3_form b3_form--lostpass" action="<?php echo b3_get_current_url(); ?>" method="post">
         <?php do_action( 'b3_render_form_element', 'lostpassword/hidden-fields', $attributes ); ?>
