2c146e08d780482679addd417b594c200e3b9bd0
Beee <info@berryplasman.com>
2026-05-25 19:22:18 +0200

    Use wp_filesystem

--- includes/download.php
+++ includes/download.php
@@ -16,9 +16,17 @@
             $file_name = sanitize_file_name( wp_unslash( $_GET[ 'file' ] ) );
 
             if ( file_exists( $file_name ) && is_readable( $file_name ) ) {
+                global $wp_filesystem;
+                if ( empty( $wp_filesystem ) ) {
+                    require_once ABSPATH . 'wp-admin/includes/file.php';
+                    WP_Filesystem();
+                }
+
                 header( "Content-Type: application/octet-stream" );
                 header( "Content-Disposition: attachment; filename={$file_name}" );
-                readfile( $file_name );
+
+                echo $wp_filesystem->get_contents( $file_name );
+                exit; // It is good practice to exit after a file download
             }
         }
     }
