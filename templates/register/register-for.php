623547e0c0f7d31d23595aa8cd360e23d97a0b62
Beee <info@berryplasman.com>
2026-06-08 22:57:48 +0200

    Optimize labels

--- templates/register/register-for.php
+++ templates/register/register-for.php
@@ -11,11 +11,11 @@
     <div>
         <input id="signupblog" type="radio" name="signup_for" value="blog" checked="checked">
         <label class="checkbox" for="signupblog">
-            <?php echo esc_html( apply_filters( 'b3_signup_for_site', esc_attr__( 'A site', 'b3-onboarding' ) ) ); ?>
+            <?php echo esc_html( apply_filters( 'b3_signup_for_site', esc_html__( 'A site', 'b3-onboarding' ) ) ); ?>
         </label>
         <input id="signupuser" type="radio" name="signup_for" value="user">
         <label class="checkbox" for="signupuser">
-            <?php echo esc_html( apply_filters( 'b3_signup_for_user', esc_attr__( 'Just a user', 'b3-onboarding' ) ) ); ?>
+            <?php echo esc_html( apply_filters( 'b3_signup_for_user', esc_html__( 'Just a user', 'b3-onboarding' ) ) ); ?>
         </label>
     </div>
 </div>
