409a657cc1b1d6f3238f817c7f05b4deb8eb7121
Beee <info@berryplasman.com>
2026-06-09 22:54:23 +0200

    Add gitignore

--- templates/login/hidden-fields.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if ( ! defined( 'ABSPATH' ) ) {
-        exit;
-    }
-?>
-<input name="b3_form" value="login" type="hidden" />
-<?php if ( isset( $attributes[ 'redirect' ] ) ) { ?>
-    <input type="hidden" name="redirect_to" value="<?php echo esc_attr( $attributes[ 'redirect' ] ); ?>">
-<?php } ?>
--- templates/magiclink/hidden-fields.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if ( ! defined( 'ABSPATH' ) ) {
-        exit;
-    }
-?>
-<input name="b3_form" value="magiclink" type="hidden" />
-<input name="<?php echo esc_attr( $attributes[ 'nonce_id' ] ); ?>" value="<?php echo esc_attr( $attributes[ 'nonce' ] ); ?>" type="hidden" />
