19e5ced0a6d4427056077f910fa4b5e5b26b1c6a
Beee <info@berryplasman.com>
2026-06-09 19:56:41 +0200

    Revert download

--- includes/download.php
+++ includes/download.php
@@ -1,6 +1,4 @@
 <?php
-    if ( ! defined( 'ABSPATH' ) ) exit;
-
     /**
      * Function to save a file
      *
@@ -12,21 +10,13 @@
             'default-email-template.html',
         ];
 
-        if ( in_array( sanitize_file_name( wp_unslash( $_GET[ 'file' ] ) ), $allowed_files ) ) {
-            $file_name = sanitize_file_name( wp_unslash( $_GET[ 'file' ] ) );
+        if ( in_array( $_GET[ 'file' ], $allowed_files ) ) {
+            $file_name = $_GET[ 'file' ];
 
             if ( file_exists( $file_name ) && is_readable( $file_name ) ) {
-                global $wp_filesystem;
-                if ( empty( $wp_filesystem ) ) {
-                    require_once ABSPATH . 'wp-admin/includes/file.php';
-                    WP_Filesystem();
-                }
-
                 header( "Content-Type: application/octet-stream" );
                 header( "Content-Disposition: attachment; filename={$file_name}" );
-
-                echo wp_kses_post( $wp_filesystem->get_contents( $file_name ) );
-                exit; // It is good practice to exit after a file download
+                readfile( $file_name );
             }
         }
     }
