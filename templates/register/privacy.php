2ee103baaff514c09321add42d2e08a7420076d7
Beee <info@berryplasman.com>
2026-06-09 18:54:34 +0200

    Escape privacy link

--- templates/register/privacy.php
+++ templates/register/privacy.php
@@ -7,7 +7,17 @@
     $label = htmlspecialchars_decode( apply_filters( 'b3_privacy_text', b3_get_privacy_text() ) );
     ?>
     <div class="b3_form-element b3_form-element--privacy">
-        <label class="b3_form-label"><?php echo esc_html( $label ); ?></label>
-        <?php echo $input; ?>
+        <label for="b3_privacy_accept" class="b3_form-label"><?php echo $label; ?></label>
+        <?php
+            $allowed = [
+                'input' => [
+                    'name'  => [],
+                    'type'  => [],
+                    'id'    => [],
+                    'value' => [],
+                ],
+            ];
+            echo wp_kses( $input, $allowed );
+        ?>
     </div>
 
