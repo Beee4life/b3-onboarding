187a1f23e0f0f6c7711884fc05cb304219792ae7
Beee <info@berryplasman.com>
2026-06-09 18:18:48 +0200

    Test unescaped

--- includes/tabs/tab-emails.php
+++ includes/tabs/tab-emails.php
@@ -62,7 +62,8 @@
             <?php } ?>
 
             <?php foreach( $email_boxes as $box ) { ?>
-                <?php echo esc_textarea( b3_render_email_settings_field( $box ) ); ?>
+                <?php echo b3_render_email_settings_field( $box ); ?>
+                <?php //echo esc_textarea( b3_render_email_settings_field( $box ) ); ?>
             <?php } ?>
 
             <?php b3_get_submit_button(); ?>
