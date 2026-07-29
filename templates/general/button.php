ea604e7fbbf05e033da6fbb186cc9b2919570fbc
Beee <info@berryplasman.com>
2026-07-29 21:49:55 +0200

    Remove unused values

--- templates/general/button.php
+++ templates/general/button.php
@@ -7,8 +7,6 @@
     $button_value    = isset( $attributes[ 'button_value' ] ) ? $attributes[ 'button_value' ] : esc_attr__( 'Save', 'b3-onboarding' );
 
     if ( 'magic-password' == $attributes[ 'template' ] ) {
-        $button_modifier1 = $button_modifier;
-        $button_value1    = $button_value;
         $button_modifier2 = isset( $attributes[ 'button_modifier2' ] ) ? $attributes[ 'button_modifier2' ] : false;
         $button_value2    = isset( $attributes[ 'button_value2' ] ) ? $attributes[ 'button_value2' ] : '';
     }
