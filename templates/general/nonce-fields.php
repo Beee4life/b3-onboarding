ce36e9ae9231b91edf5529634ed16ce3dd4af1e1
Beee <info@berryplasman.com>
2026-07-29 21:50:00 +0200

    Ad dblank

--- templates/general/nonce-fields.php
+++ templates/general/nonce-fields.php
@@ -2,6 +2,7 @@
     if ( ! defined( 'ABSPATH' ) ) {
         exit;
     }
+
     $nonce_key = 'magic-password' == $attributes[ 'template' ] ? 'magiclink' : $attributes[ 'template' ];
     $action    = sprintf( 'b3_%s_nonce', $nonce_key );
     $key       = sprintf( 'b3_%s', $nonce_key );
