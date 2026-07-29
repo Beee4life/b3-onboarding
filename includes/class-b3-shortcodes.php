89463ee6e5ed7fdebd361bf41a30eb8376242e6f
Beee <info@berryplasman.com>
2026-07-29 21:54:23 +0200

    Change var

--- includes/class-b3-shortcodes.php
+++ includes/class-b3-shortcodes.php
@@ -240,9 +240,9 @@
                     $attributes[ 'button_value' ] = esc_attr__( 'Get magic link', 'b3-onboarding' );
                     $attributes[ 'form_action' ]  = b3_get_login_url();
                     $attributes[ 'template' ]     = 'magiclink';
-                    $use_both                     = get_option( 'b3_use_magic_link_password' );
+                    $use_magiclink_and_password   = get_option( 'b3_use_magic_link_password' );
 
-                    if ( $use_both ) {
+                    if ( $use_magiclink_and_password ) {
                         $attributes[ 'button_modifier2' ] = esc_attr__( 'use-password', 'b3-onboarding' );
                         $attributes[ 'button_value2' ]    = esc_attr__( 'Use password', 'b3-onboarding' );
                         $attributes[ 'template' ]         = 'magic-password';
