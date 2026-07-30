08c405afc334129c538272afdbe137a9b65ef443
Beee <info@berryplasman.com>
2026-07-30 15:05:42 +0200

    Don't check if restriction is active

--- includes/do-stuff.php
+++ includes/do-stuff.php
@@ -333,7 +333,7 @@
     function b3_verify_email_domain( $email ) {
         $disallowed_domains = b3_get_disallowed_domain_names();
 
-        if ( get_option( 'b3_activate_domain_restriction' ) && ! empty( $disallowed_domains ) ) {
+        if ( ! empty( $disallowed_domains ) ) {
             $domain_name = substr( strrchr( $email, '@' ), 1 );
 
             if ( $domain_name && in_array( $domain_name, $disallowed_domains ) ) {
