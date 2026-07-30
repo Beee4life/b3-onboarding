0e6a9943ea2b03f5492f4d2992d1dae2f38de84b
Beee <info@berryplasman.com>
2026-07-30 13:44:00 +0200

    Render in indiv. files

--- templates/lostpassword.php
+++ templates/lostpassword.php
@@ -16,11 +16,10 @@
         <?php echo sprintf( '<h3>%s</h3>', esc_html( $attributes[ 'title' ] ) ); ?>
     <?php } ?>
 
-    <form name="lostpasswordform" id="lostpasswordform" class="b3_form b3_form--lostpass" action="<?php echo esc_url( b3_get_lostpassword_url() ); ?>" method="post">
-        <?php do_action( 'b3_render_form_element', 'general/nonce-fields', $attributes ); ?>
-        <?php do_action( 'b3_render_form_element', 'lostpassword/hidden-fields', $attributes ); ?>
-        <?php do_action( 'b3_render_form_element', 'lostpassword/user-login', $attributes ); ?>
-        <?php do_action( 'b3_render_form_element', 'general/button', $attributes ); ?>
-        <?php do_action( 'b3_add_action_links', $attributes[ 'template' ] ); ?>
-    </form>
+    <?php if ( get_option( 'b3_use_magic_link' ) ) { ?>
+        <?php $attributes[ 'template' ] = 'magiclink'; ?>
+        <?php include 'magiclink/magiclink-form.php'; ?>
+    <?php } else { ?>
+        <?php include 'lostpassword/password-form.php'; ?>
+    <?php } ?>
 </div>
--- templates/magiclink.php
+++ templates/magiclink.php
@@ -16,10 +16,5 @@
 <div id="b3-resetpass" class="b3 b3_page b3_page--magiclink">
     <?php echo ( isset( $attributes[ 'title' ] ) ) ? sprintf( '<h3>%s</h3>', esc_html( $attributes[ 'title' ] ) ) : false; ?>
 
-    <form name="magiclinkform" id="magiclinkform" action="<?php echo esc_attr( $attributes[ 'form_action' ] ); ?>" method="post" autocomplete="off">
-        <?php do_action( 'b3_render_form_element', 'general/nonce-fields', $attributes ); ?>
-        <?php do_action( 'b3_render_form_element', 'magiclink/user-email', $attributes ); ?>
-        <?php do_action( 'b3_render_form_element', 'general/button', $attributes ); ?>
-        <?php do_action( 'b3_add_action_links', $attributes[ 'template' ] ); ?>
-    </form>
+    <?php include 'magiclink/magiclink-form.php'; ?>
 </div>
