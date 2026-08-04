81619874b75978eb79e6fc9c9d27b21afe96d040
Beee <info@berryplasman.com>
2026-08-04 18:13:06 +0200

    Improve escaping

--- admin/tabs/tab-emails.php
+++ admin/tabs/tab-emails.php
@@ -62,7 +62,7 @@
             <?php } ?>
 
             <?php foreach( $email_boxes as $box ) { ?>
-                <?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+                <?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Output is pre-escaped via ob_start inside b3_render_email_settings_field(). ?>
                 <?php echo b3_render_email_settings_field( $box ); ?>
             <?php } ?>
 
