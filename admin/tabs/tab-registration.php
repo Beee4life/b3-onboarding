4f877fb13b4f59a2a4ae9894c7ffc44a96fd16ec
Beee <info@berryplasman.com>
2026-08-08 17:50:38 +0200

    Show first name fields

--- admin/tabs/tab-registration.php
+++ admin/tabs/tab-registration.php
@@ -116,8 +116,7 @@
                         <?php b3_get_close(); ?>
                     <?php } ?>
 
-                    <?php $hide_extended_fields = $registration_with_email_only ? ' hidden' : false; ?>
-                    <div class="b3-name-fields<?php echo esc_attr( $hide_extended_fields ); ?>">
+                    <div class="b3-name-fields">
                         <?php b3_get_settings_field_open(); ?>
                             <?php b3_get_label_field_open(); ?>
                                 <label for="b3_activate_first_last"><?php esc_html_e( 'First and last name', 'b3-onboarding' ); ?></label>
