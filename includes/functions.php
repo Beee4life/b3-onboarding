bf9635da32bc1285462e33d4ab64886fd92279c4
Beee <info@berryplasman.com>
2026-08-09 15:46:26 +0200

    Remove option 'open'

--- includes/functions.php
+++ includes/functions.php
@@ -132,10 +132,6 @@
                 'value' => 'email_activation',
                 'label' => esc_html__( 'Open (user needs to confirm email)', 'b3-onboarding' ),
             ],
-            // [
-            //     'value' => 'open',
-            //     'label' => esc_html__( 'Open (user is instantly active)', 'b3-onboarding' ),
-            // ],
         ];
 
         $multisite_options = [
