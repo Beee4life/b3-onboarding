962f2390f5f190243bda820e2b734d65bc3f13d9
Beee <info@berryplasman.com>
2026-08-10 20:21:25 +0200

    Remove comments

--- includes/filters-wp.php
+++ includes/filters-wp.php
@@ -234,8 +234,6 @@
 
     // add custom meta for manually added users in MU
     function b3_add_admin_created_signup_meta( $meta ) {
-        // @TODO: test in single site as well
-        // @TODO: test if not done twice
         $is_admin_form = is_admin() || ( isset( $_POST[ '_wp_http_referer' ] ) && false !== strpos( $_POST[ '_wp_http_referer' ], 'user-new.php' ) );
 
         if ( $is_admin_form ) {
