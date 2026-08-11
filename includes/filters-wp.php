56b12fa323368871a465698cfbe3f57a8cf86d31
Beee <info@berryplasman.com>
2026-08-11 13:31:58 +0200

    Add back

--- includes/filters-wp.php
+++ includes/filters-wp.php
@@ -102,6 +102,12 @@
 
             if ( $admin_approval && in_array( 'b3_approval', (array) $user_object->roles ) ) {
                 unset( $actions[ 'resetpassword' ] );
+                $actions[ 'activate' ] = sprintf( '<a href="%1$s">%2$s</a>',
+                    add_query_arg( 'wp_http_referer', $request_uri,
+                        wp_nonce_url( 'users.php?action=activate&amp;user_id=' . $user_object->ID, 'manual-activation' )
+                    ),
+                    esc_attr__( 'Activate', 'b3-onboarding' )
+                );
 
             } elseif ( 'email_activation' === $registration_type ) {
                 if ( in_array( 'b3_activation', (array) $user_object->roles ) ) {
